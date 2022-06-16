cf7baf23253c64c0a65c8a7074a9971a7a40dc66
shyamsulbhewar7 <shyamsulbhewar@gmail.com>
2022-06-17 01:23:56 +0530

    Pig Dice Game

---
+++ Model/PigDice.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigDiceOOP.Model
+{
+    public class PigDice
+    {
+        private int _totalscore;
+        public PigDice()
+        {
+            _totalscore = 0;
+        }
+        public int Play()
+        {
+            Random rnd = new Random();
+            int turns = 1;
+            int current = 0;
+            while (_totalscore < 21)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("TURN " + turns);
+                turns++;
+                current = 0;
+                while (true)
+                {
+                    Console.WriteLine("Roll or Hold ? ( r/h ) : ");
+                    char p;
+                    p = Console.ReadLine()[0];
+                    if (p == 'r')
+                    {
+                        int number = rnd.Next(1, 7);
+                        Console.WriteLine("Die : " + number);
+                        if (number > 1)
+                        {
+                            current += number;
+                        }
+                        else
+                        {
+                            current = 0;
+                            Console.WriteLine("Turn Over. No Score. ");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        _totalscore += current;
+                        break;
+                    }
+                }
+                Console.WriteLine("Total Score :" + _totalscore);
+                Console.WriteLine("Score for this turn :" + current);
+            }
+            return turns;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using PigDiceOOP.Model;
+using System;
+
+namespace PigDiceOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before starting the game , lets get to know some rules :");
+            Console.WriteLine("1) See how many turns it takes you to get to 20.");
+            Console.WriteLine("2) Turn ends when you hold or roll a 1.");
+            Console.WriteLine("3) If you roll a 1, you lose all points for the turn");
+            Console.WriteLine("4) If you hold, you save all points for the turn");
+            PigDice player1 = new PigDice();
+            Console.WriteLine("Player took " + player1.Play() + " Turns to reach target");
+        }
+    }
+}
